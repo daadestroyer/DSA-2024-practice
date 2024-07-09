b2d073d1b2311885802afc7954e77bde6b2aaba4
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-09 15:39:56 +0530

    armstrong number program added...

---
+++ src/BasicMaths/App06_ArmstrongNo.java
@@ -0,0 +1,21 @@
+package BasicMaths;
+
+public class App06_ArmstrongNo {
+
+    static boolean findArmstrong(int num) {
+        int power = Integer.toString(num).length();
+        int originalNo = num;
+        int armstrongNo = 0;
+        while(num != 0){
+            int lastDigit = num % 10;
+            armstrongNo += Math.pow(lastDigit,power);
+            num /= 10;
+        }
+        return armstrongNo == originalNo ? true : false;
+    }
+
+    public static void main(String[] args) {
+        int num = 153;
+        System.out.println(findArmstrong(num));
+    }
+}
