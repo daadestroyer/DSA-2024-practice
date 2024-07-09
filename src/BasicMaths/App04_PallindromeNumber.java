5b8288f64bd577a8ac84f84a40fbdab0ebf2273a
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-09 13:19:22 +0530

    number is pallindrome or not added...

---
+++ src/BasicMaths/App04_PallindromeNumber.java
@@ -0,0 +1,23 @@
+package BasicMaths;
+
+public class App04_PallindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        int pallindrome_no = 0;
+        int original_no;
+        while (x != 0) {
+            int lastDigit = x % 10;
+            pallindrome_no = pallindrome_no * 10 + lastDigit;
+            x = x / 10;
+            if (pallindrome_no > Integer.MAX_VALUE || pallindrome_no < Integer.MIN_VALUE) {
+                return false;
+            }
+        }
+        return original_no == pallindrome_no ? true : false;
+    }
+
+    public static void main(String[] args) {
+        App04_PallindromeNumber app = new App04_PallindromeNumber();
+
+    }
+}
