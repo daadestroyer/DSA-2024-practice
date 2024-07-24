72e66e13153d9bde316ea318436de95d850d3397
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-07-24 12:22:02 +0530

    move zeroes at end completed...

---
+++ src/Arrays_06/Basic/App06_MoveZeroesAtEnd.java
@@ -0,0 +1,23 @@
+package Arrays_06.Basic;
+
+import java.util.Arrays;
+
+public class App06_MoveZeroesAtEnd {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        int n = nums.length;
+
+        int ptr = 0;
+        for (int i : nums) {
+            if (i != 0) {
+                nums[ptr] = i;
+                ptr++;
+            }
+        }
+        while (ptr < nums.length) {
+            nums[ptr] = 0;
+            ptr++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
