fdf20ce4d73dc9f8842fc6927cf160bc2070363b
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-08-01 12:23:24 +0530

    Maximum Subarray / Kadane Algo completed...

---
+++ src/Arrays_06/Medium/App05_MaximumSubArray_KadanesAlgorithm.java
@@ -0,0 +1,25 @@
+package Arrays_06.Medium;
+
+public class App05_MaximumSubArray_KadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int res = maximumSubArray(arr);
+        System.out.println(res);
+    }
+
+    private static int maximumSubArray(int[] arr) {
+        int csum = Integer.MIN_VALUE;
+        int osum = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (csum >= 0) {
+                csum += i;
+            } else {
+                csum = i;
+            }
+            if (csum > osum) {
+                osum = csum;
+            }
+        }
+        return osum;
+    }
+}
