0f9f648f21fcaa01394557d748a93c076fe2a2a7
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-08-01 13:27:41 +0530

    best time to buy and sell stocks - 1

---
+++ src/Arrays_06/Basic/App11_BestTimeToBuyAndSellStock_1.java
@@ -0,0 +1,19 @@
+package Arrays_06.Basic;
+
+public class App11_BestTimeToBuyAndSellStock_1 {
+    public static void main(String[] args) {
+        int[] price = {7, 1, 5, 3, 6, 4};
+        // if i purchase on day 2 at price 1 and sell at day 5 at 6 then profit will be 5 which is max
+        // 1 <= prices.length <= 105
+        // 0 <= prices[i] <= 104
+
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i : price) {
+            minPrice = Math.min(i, minPrice);
+            maxProfit = Math.max(maxProfit, i - minPrice);
+        }
+        System.out.println(maxProfit);
+    }
+}
