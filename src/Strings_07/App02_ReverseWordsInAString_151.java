c24062c0114cfa6b78f9a2c312d93bae0110a6e7
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-09-12 22:48:45 +0530

    ReverseWordsInAString_151...

---
+++ src/Strings_07/App02_ReverseWordsInAString_151.java
@@ -0,0 +1,21 @@
+package Strings_07;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App02_ReverseWordsInAString_151 {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        String[] str = s.trim().split("\\s+"); // --> \\s means skipping one white space
+        // --> \\s+ means skipping all white space after any word
+        List<String> lst = Arrays.asList(str);
+        Collections.reverse(lst);
+        String res = String.join(" ", lst); // here join method means we are return string with some space
+        // after each work from the given data structure
+
+        System.out.println(res);
+    }
+}
