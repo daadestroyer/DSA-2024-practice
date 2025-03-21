b41751c753a832e8c507cde475439874b011f211
daadestroyer <nigamshubham2000@gmail.com>
2025-03-22 02:01:53 +0530

    linkedin list code added...

---
+++ src/LinkedList_09/Problems/App06_ReverseLinkedList_Recursive.java
@@ -0,0 +1,19 @@
+package LinkedList_09.Problems;
+
+public class App06_ReverseLinkedList_Recursive {
+    public static void main(String[] args) {
+        /*
+            public static Node reverseLinkedList(Node head) {
+                if(head == null || head.next == null){
+                    return head;
+                }
+
+                Node newhead = reverseLinkedList(head.next);
+                Node fast = head.next;
+                fast.next = head;
+                head.next = null;
+                return newhead;
+            }
+        */
+    }
+}
