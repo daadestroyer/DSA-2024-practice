5ba472123e8165aa8dffeb0324553cc3d52eac48
daadestroyer <nigamshubham2000@gmail.com>
2025-10-14 13:38:42 +0530

    code added.

--- src/Sorting_05/App06_MergeSort_INPLACE.java
+++ src/Sorting_05/App06_MergeSort_INPLACE.java
@@ -10,10 +10,14 @@ public class App06_MergeSort_INPLACE {
         int[] left = new int[n1];
         int[] right = new int[n2];
 
-        for (int i = 0; i < n1; i++) left[i] = arr[low + i];
-        for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[low + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
 
-        int i = 0, j = 0, k = low; // <-- k must start at low
+        int i = 0, j = 0, k = low;
         while (i < n1 && j < n2) {
             if (left[i] <= right[j]) {
                 arr[k++] = left[i++];
