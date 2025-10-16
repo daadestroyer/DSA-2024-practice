7367cee3b17bb6ba0dfedee32b75cfe6801d34aa
daadestroyer <nigamshubham2000@gmail.com>
2025-10-16 23:10:43 +0530

    @Primary and @ConditionalOnProperty...

---
+++ src/DynamicProgramming_15/DP_ON_LIS_06/App01_LongestIncreasingSubSequence_300.java
@@ -0,0 +1,34 @@
+package DynamicProgramming_15.DP_ON_LIS_06;
+
+import java.util.Arrays;
+
+public class App01_LongestIncreasingSubSequence_300 {
+    public static int findLIS(int idx, int prev_idx, int[] nums, int[][] dp) {
+        if (idx == nums.length) {
+            return 0;
+        }
+        int res = 0;
+        // not pick
+        int len1 = findLIS(idx + 1, prev_idx, nums, dp);
+
+        // pick
+        int len2 = 0;
+        if (prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+            len2 = 1 + findLIS(idx + 1, idx, nums, dp);
+        }
+        res = Math.max(len1, len2);
+        dp[idx][prev_idx + 1] = res;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        int n = nums.length;
+        int[][] dp = new int[n][n + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        int len = findLIS(0, -1, nums, dp);
+        System.out.println(len);
+    }
+}
